394c29026f21ffe1dc918e106d44ba9b923f9d3b
hatscat <luboudy@gmail.com>
2018-02-17 11:46:15 +0100

    add blockly + css dependencies

---
+++ DEVSETUP.md
@@ -0,0 +1,17 @@
+# Development Environment Setup
+
+## Install NodeJs, Elm and setup your IDE
+read this: [https://github.com/knowthen/elm/blob/master/DEVSETUP.md](https://github.com/knowthen/elm/blob/master/DEVSETUP.md)
+
+## Install "Create Elm App"
+`npm install create-elm-app -g`
+[more infos](https://github.com/halfzebra/create-elm-app/blob/master/README.md#create-elm-app---)
+
+## Install dependencies
+`npm install`
+`elm-app install`
+`elm-app build`
+
+## Launch the application
+`elm-app test`
+`elm-app start`
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import 'semantic-ui-css/semantic.min.css';
+// import Blockly from 'blockly/blockly_compressed.js';
+import 'script-loader!blockly/blockly_compressed.js';
+import 'script-loader!blockly/blocks_compressed.js'
+import 'script-loader!blockly/msg/js/fr.js'
 import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
@@ -5,3 +10,5 @@ import registerServiceWorker from './registerServiceWorker';
 Main.embed(document.getElementById('root'));
 
 registerServiceWorker();
+
+console.log(Blockly)
