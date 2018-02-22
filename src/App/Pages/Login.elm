d60b8f7fce6687969d1bc278d62f8609a65271d9
hatscat <luboudy@gmail.com>
2018-02-22 22:31:31 +0100

    formating

--- src/App/Pages/Login.elm
+++ src/App/Pages/Login.elm
@@ -1,11 +1,11 @@
 module App.Pages.Login exposing (loginView)
 
-import App.Messages exposing (Msg(TeamName, ChangePage, NoOp))
+import App.Messages exposing (Msg(ChangePage, NoOp, TeamName))
 import App.Model exposing (Page(Overview))
-import Utils exposing(viewIf)
-import Html exposing (Html, button, div, h1, img, text, input, form)
-import Html.Attributes exposing (src, placeholder, type_, value)
-import Html.Events exposing (onSubmit, onInput)
+import Html exposing (Html, button, div, form, h1, img, input, text)
+import Html.Attributes exposing (placeholder, src, type_, value)
+import Html.Events exposing (onInput, onSubmit)
+import Utils exposing (viewIf)
 
 
 loginView : Int -> Html Msg
@@ -13,8 +13,15 @@ loginView teamNameLength =
     div []
         [ img [ src "/logo.svg" ] []
         , h1 [] [ text "La chasse aux bugs !" ]
-        , form [ onSubmit ( if teamNameLength > 2 then ChangePage Overview else NoOp ) ] [
-            input [ type_ "text", placeholder "Nom d'équipe", onInput TeamName ] []
+        , form
+            [ onSubmit
+                (if teamNameLength > 2 then
+                    ChangePage Overview
+                 else
+                    NoOp
+                )
+            ]
+            [ input [ type_ "text", placeholder "Nom d'équipe", onInput TeamName ] []
             , viewIf (teamNameLength > 2) (input [ type_ "submit", value "valider" ] [])
             ]
         ]
