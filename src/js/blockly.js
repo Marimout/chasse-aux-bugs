eb174ad7236b746085024b034b1fbd14739d94d0
hatscat <luboudy@gmail>
2018-07-18 09:04:15 +0000

    fix bockly injection

--- src/js/blockly.js
+++ src/js/blockly.js
@@ -1,3 +1,4 @@
+// @format
 // import 'script-loader!blockly/blockly_compressed.js';
 // import 'script-loader!blockly/blocks_compressed.js';
 // import 'script-loader!blockly/javascript_compressed.js';
@@ -6,42 +7,69 @@
 export default function injectBlockly(args) {
   var [divId, toolbox] = args;
 
-  console.log("divId", divId, document.getElementById(divId));
-//   console.log("Blockly", Blockly);
+  let initBlocklyWorkspace = () => {
+    let blocklyDiv = document.getElementById(divId);
 
-  window.workspace = Blockly.inject(document.getElementById(divId), {
-        toolbox: toolbox
-      });
+    if (!blocklyDiv) {
+      console.log('ERROR: no ' + divId);
+      setTimeout(initBlocklyWorkspace, 200);
+      return;
+    }
+
+    console.log('divId', divId, blocklyDiv);
+
+    blocklyDiv.style.height = '100%';
+
+    window.workspace = Blockly.inject(blocklyDiv, {
+      toolbox: toolbox,
+    });
 
     initReplaceBlock();
+  };
+
+  initBlocklyWorkspace();
 }
 
 export function initReplaceBlock() {
-    Blockly.Blocks['text_replace'] = {
-        init: function () {
-            this.appendValueInput("in")
-                .setCheck("String")
-                .appendField("dans le texte");
-            this.appendValueInput("from")
-                .setCheck("String")
-                .appendField("remplacer tout");
-            this.appendValueInput("to")
-                .setCheck("String")
-                .appendField("par");
-            this.setInputsInline(true);
-            this.setOutput(true, "String");
-            this.setColour(165);
-            this.setTooltip("renvois un nouveau texte");
-            this.setHelpUrl("https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/replace");
-        }
-    };
-    Blockly.JavaScript['text_replace'] = function (block) {
-        var value_in = Blockly.JavaScript.valueToCode(block, 'in', Blockly.JavaScript.ORDER_ATOMIC);
-        var value_from = Blockly.JavaScript.valueToCode(block, 'from', Blockly.JavaScript.ORDER_ATOMIC);
-        var value_to = Blockly.JavaScript.valueToCode(block, 'to', Blockly.JavaScript.ORDER_ATOMIC);
-        // TODO: Assemble JavaScript into code variable.
-        var code = value_in.replace(value_from, value_to);
-        // TODO: Change ORDER_NONE to the correct strength.
-        return [code, Blockly.JavaScript.ORDER_MEMBER];
-    };
+  Blockly.Blocks['text_replace'] = {
+    init: function() {
+      this.appendValueInput('in')
+        .setCheck('String')
+        .appendField('dans le texte');
+      this.appendValueInput('from')
+        .setCheck('String')
+        .appendField('remplacer tout');
+      this.appendValueInput('to')
+        .setCheck('String')
+        .appendField('par');
+      this.setInputsInline(true);
+      this.setOutput(true, 'String');
+      this.setColour(165);
+      this.setTooltip('renvois un nouveau texte');
+      this.setHelpUrl(
+        'https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/String/replace',
+      );
+    },
+  };
+  Blockly.JavaScript['text_replace'] = function(block) {
+    var value_in = Blockly.JavaScript.valueToCode(
+      block,
+      'in',
+      Blockly.JavaScript.ORDER_ATOMIC,
+    );
+    var value_from = Blockly.JavaScript.valueToCode(
+      block,
+      'from',
+      Blockly.JavaScript.ORDER_ATOMIC,
+    );
+    var value_to = Blockly.JavaScript.valueToCode(
+      block,
+      'to',
+      Blockly.JavaScript.ORDER_ATOMIC,
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = value_in.replace(value_from, value_to);
+    // TODO: Change ORDER_NONE to the correct strength.
+    return [code, Blockly.JavaScript.ORDER_MEMBER];
+  };
 }
